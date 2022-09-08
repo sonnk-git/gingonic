157e735f2eaea3cd4b69b6e8dbaeb39daee731a1
SonNK <sonnk@miichisoft.com>
2022-09-08 23:41:49 +0700

    init

---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/toorop/gin-logrus"
+	"github.com/foolin/goview/supports/ginview"
+)
+
+var db = make(map[string]string)
+
+func setupRouter() *gin.Engine {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	gin.SetMode(os.Getenv("MODE"))
+	r := gin.Default()
+
+	log := logrus.New()
+	r.Use(ginlogrus.Logger(log), gin.Recovery())
+
+	// The API for setting attributes is a little different than the package level
+	// exported logger. See Godoc.
+	log.Out = os.Stdout
+
+	// You could set this to any `io.Writer` such as a file
+	file, err := os.OpenFile("logrus.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+	 log.Out = file
+	} else {
+	 log.Info("Failed to log to file, using default stderr")
+	}
+
+	r.HTMLRender = ginview.Default()
+
+	r.GET("/page", func(ctx *gin.Context) {
+		//render only file, must full name with extension
+		ctx.HTML(http.StatusOK, "page.html", gin.H{"title": "Page file title!!"})
+	})
+
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	// Get user value
+	r.GET("/user/:name", func(c *gin.Context) {
+		user := c.Params.ByName("name")
+		value, ok := db[user]
+		if ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+		}
+	})
+
+	// Authorized group (uses gin.BasicAuth() middleware)
+	// Same than:
+	// authorized := r.Group("/")
+	// authorized.Use(gin.BasicAuth(gin.Credentials{
+	//	  "foo":  "bar",
+	//	  "manu": "123",
+	//}))
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		"foo":  "bar", // user:foo password:bar
+		"manu": "123", // user:manu password:123
+	}))
+
+	/* example curl for /admin with basicauth header
+	   Zm9vOmJhcg== is base64("foo:bar")
+
+		curl -X POST \
+	  	http://localhost:8080/admin \
+	  	-H 'authorization: Basic Zm9vOmJhcg==' \
+	  	-H 'content-type: application/json' \
+	  	-d '{"value":"bar"}'
+	*/
+	authorized.POST("admin", func(c *gin.Context) {
+		user := c.MustGet(gin.AuthUserKey).(string)
+
+		// Parse JSON
+		var json struct {
+			Value string `json:"value" binding:"required"`
+		}
+
+		if c.Bind(&json) == nil {
+			db[user] = json.Value
+			c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		}
+	})
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
