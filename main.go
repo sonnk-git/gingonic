f2dc1fe411271c0cf3f8c2b07f7a941b989fa8cc
SonNK <sonnk@miichisoft.com>
2022-10-26 07:38:30 +0700

    refactor router

--- main.go
+++ main.go
@@ -2,84 +2,11 @@ package main
 
 import (
 	"gingonic/route"
-	"github.com/joho/godotenv"
-	"gorm.io/gorm"
-	"log"
-	"net/http"
-	"os"
-
-	"gingonic/db"
-	"gingonic/middlewares"
-	"gingonic/models"
-	"github.com/foolin/goview/supports/ginview"
-	"github.com/gin-gonic/gin"
 )
 
-var DB *gorm.DB
-
-func setupRouter() *gin.Engine {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	// Disable Console Color
-	// gin.DisableConsoleColor()
-	gin.SetMode(os.Getenv("MODE"))
-	r := gin.Default()
-	r = middlewares.SetUpLogger(r)
-
-	privateRouter := r.Group("/api")
-	privateRouter.Use(middlewares.JwtTokenCheck)
-
-	DB = db.InitORM()
-	err = models.AutoMigrate(DB)
-	if err != nil {
-		log.Fatal("Error migrate DB")
-	}
-
-	r.HTMLRender = ginview.Default()
-	route.RegisterWeb(r)
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	authorized.POST("admin", func(c *gin.Context) {
-		//user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
-	})
-
-	return r
-}
 
 func main() {
-	r := setupRouter()
+	r := route.SetupRouter()
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
